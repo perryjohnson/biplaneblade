863ace84ddd35ccd8c958d067b816e73756ac965
Perry Roth-Johnson <perryjohnson@ucla.edu>
2013-08-16 13:54:56 -0700

    removed station 22 (just outboard of midblade joint), added two evenly-spaced stations just outboard of station 21

--- insert_station.py
+++ insert_station.py
@@ -32,9 +32,11 @@ b1 = bl.BiplaneBlade(
 # select the inboard and outboard endpoints
 i_endpt = 21
 o_endpt = 23
-# select airfoil, with given t/c
-af_name = 'DU91-W2-250'
-af_t_to_c = 0.25
+# select a fraction distance to move between the endpoint stations
+x_new_frac = 2.0/3.0
+# # select airfoil, with given t/c
+# af_name = 'DU91-W2-250'
+# af_t_to_c = 0.25
 ### ---------------------------------------------------------------------------
 
 ### CRUNCH THE NUMBERS
@@ -48,39 +50,42 @@ t_i_stn = i_stn['thickness-to-chord ratio']*i_stn['chord']
 x_o_stn = o_stn['x1']
 c_o_stn = o_stn['chord']
 t_o_stn = o_stn['thickness-to-chord ratio']*o_stn['chord']
-# define eqns for c_blade, t_blade, and t_af
-c_blade = ipl.interp1d(x=[x_i_stn, x_o_stn],
-                       y=[c_i_stn, c_o_stn])
-t_blade = ipl.interp1d(x=[x_i_stn, x_o_stn],
-                       y=[t_i_stn, t_o_stn])
+# calculate the new x1-location
+x_new_stn = x_new_frac*(x_o_stn-x_i_stn) + x_i_stn
+# define eqns for blade chord, blade thickness, and airfoil thickness
+# blade chord
+chord = ipl.interp1d(x=[x_i_stn, x_o_stn], y=[c_i_stn, c_o_stn])
+# blade thickness
+thickness = ipl.interp1d(x=[x_i_stn, x_o_stn], y=[t_i_stn, t_o_stn])
 def t_airfoil(t_to_c, c):
+    """Airfoil thickness."""
     return t_to_c*c
-# set t_blade = t_airfoil, solve for x
-def x_new(t_to_c, c_i, t_i, x_i, c_o, t_o, x_o):
-    """Find the x1-location for a new station by setting t_blade = t_airfoil.
+# # set thickness = t_airfoil, solve for x
+# def x_new(t_to_c, c_i, t_i, x_i, c_o, t_o, x_o):
+#     """Find the x1-location for a new station by setting thickness = t_airfoil.
 
-    Parameters (all floats)
-    ----------
-    t_to_c : thickness-to-chord ratio of the given airfoil
-    c_i : chord of the inboard endpoint station
-    t_i : thickness of the inboard endpoint station
-    x_i : x1-location of the inboard endpoint station
-    c_o : chord of the outboard endpoint station
-    t_o : thickness of the outboard endpoint station
-    x_o : x1-location of the outboard endpoint station
+#     Parameters (all floats)
+#     ----------
+#     t_to_c : thickness-to-chord ratio of the given airfoil
+#     c_i : chord of the inboard endpoint station
+#     t_i : thickness of the inboard endpoint station
+#     x_i : x1-location of the inboard endpoint station
+#     c_o : chord of the outboard endpoint station
+#     t_o : thickness of the outboard endpoint station
+#     x_o : x1-location of the outboard endpoint station
 
-    """
-    numer = t_to_c*c_i - t_i
-    denom = ((t_o-t_i)/(x_o-x_i) - t_to_c*(c_o-c_i)/(x_o-x_i))
-    return numer/denom + x_i
-x_new_stn = x_new(
-    t_to_c=af_t_to_c,
-    c_i=c_i_stn,
-    t_i=t_i_stn,
-    x_i=x_i_stn,
-    c_o=c_o_stn,
-    t_o=t_o_stn,
-    x_o=x_o_stn)
+#     """
+#     numer = t_to_c*c_i - t_i
+#     denom = ((t_o-t_i)/(x_o-x_i) - t_to_c*(c_o-c_i)/(x_o-x_i))
+#     return numer/denom + x_i
+# x_new_stn = x_new(
+#     t_to_c=af_t_to_c,
+#     c_i=c_i_stn,
+#     t_i=t_i_stn,
+#     x_i=x_i_stn,
+#     c_o=c_o_stn,
+#     t_o=t_o_stn,
+#     x_o=x_o_stn)
 # now, given x_new_stn, calculate all the other parameters with lin interp
 # (twist, laminate thicknesses, etc)
 twist = ipl.interp1d(
@@ -110,7 +115,8 @@ aft_panel_height = ipl.interp1d(
 print ''
 print 'x1 = {0:6.3f}'.format(x_new_stn)
 print 'twist = {0:6.3f}'.format(float(twist(x_new_stn)))
-print 'chord = {0:6.3f}'.format(float(c_blade(x_new_stn)))
+print 'chord = {0:6.3f}'.format(float(chord(x_new_stn)))
+print 'thickness-to-chord ratio = {0:6.3f}'.format(float(thickness(x_new_stn)/chord(x_new_stn)))
 print 'spar cap height = {0:6.3f}'.format(float(spar_cap_height(x_new_stn)))
 print 'shear web 3 x2 = {0:6.3f}'.format(float(SW_3_x2(x_new_stn)))
 print 'TE reinf height uniax = {0:6.3f}'.format(float(TE_reinf_height_uniax(x_new_stn)))
--- scale_airfoils.py
+++ scale_airfoils.py
@@ -5,46 +5,16 @@ airfoil_dir = 'biplane_flap-sym_no-stagger/airfoils'
 airfoil_list = []
 
 airfoil_list.append({
-    'original_path' : 'SNL-100m-Transition51.txt',
-    'original_tc_ratio' : 0.51,
-    'new_path' : 'SNL-100m-Transition51_53pt2.txt',
-    'new_tc_ratio' : 0.532
-    })
-airfoil_list.append({
-    'original_path' : 'DU99-W-405.txt',
-    'original_tc_ratio' : 0.405,
-    'new_path' : 'DU99-W-405_40pt8.txt',
-    'new_tc_ratio' : 0.408
+    'original_path' : 'DU91-W2-250_26.txt',
+    'original_tc_ratio' : 0.26,
+    'new_path' : 'DU91-W2-250_25pt1.txt',
+    'new_tc_ratio' : 0.251
     })
 airfoil_list.append({
     'original_path' : 'DU91-W2-250_26.txt',
     'original_tc_ratio' : 0.26,
-    'new_path' : 'DU91-W2-250_26_25pt5.txt',
-    'new_tc_ratio' : 0.255
-    })
-airfoil_list.append({
-    'original_path' : 'DU93-W-210_23.txt',
-    'original_tc_ratio' : 0.23,
-    'new_path' : 'DU93-W-210_23_23pt5.txt',
-    'new_tc_ratio' : 0.235
-    })
-airfoil_list.append({
-    'original_path' : 'DU93-W-210.txt',
-    'original_tc_ratio' : 0.21,
-    'new_path' : 'DU93-W-210_21pt75.txt',
-    'new_tc_ratio' : 0.2175
-    })
-airfoil_list.append({
-    'original_path' : 'DU93-W-210.txt',
-    'original_tc_ratio' : 0.21,
-    'new_path' : 'DU93-W-210_20pt25.txt',
-    'new_tc_ratio' : 0.2025
-    })
-airfoil_list.append({
-    'original_path' : 'DU93-W-210.txt',
-    'original_tc_ratio' : 0.21,
-    'new_path' : 'DU93-W-210_20pt4.txt',
-    'new_tc_ratio' : 0.204
+    'new_path' : 'DU91-W2-250_24pt1.txt',
+    'new_tc_ratio' : 0.241
     })
 
 for airfoil in airfoil_list:
