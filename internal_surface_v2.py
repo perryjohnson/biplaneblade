457138055e5f59a1dea98adf626e1e7902f285a0
Perry Roth-Johnson <perryjohnson@ucla.edu>
2013-10-09 13:05:05 -0700

    find good loops with big areas and sort by centroid x-coord

--- internal_surface_v2.py
+++ internal_surface_v2.py
@@ -11,6 +11,7 @@ reload(bl)
 from shapely.geometry import Polygon
 # from shapely.validation import explain_validity
 import matplotlib.pyplot as plt
+from operator import attrgetter
 
 # --- sandia blade ------------------------------------------------------------
 m = bl.MonoplaneBlade('Sandia blade SNL100-00', 'sandia_blade')
@@ -27,14 +28,24 @@ for station in m.list_of_stations:
     station.find_all_part_polygons()
 
 # check that parts are getting merged properly in stations that are plotting weird
-for station in m.list_of_stations[:-1]:
+for station in m.list_of_stations:
     p = station.merge_all_parts()
-    print "Station #{0} has {1} interior loops.".format(station.station_num, len(p.interiors))
+    good_loops = []
     for n, interior in enumerate(p.interiors):
-        c = interior.centroid.wkt
-        print "  Interior #{0}, centroid at {1}".format(n,c)
-# stn34 = m.list_of_stations[-1]
-# p = stn34.merge_all_parts(plot_flag=True)
-# stn34.plot_parts()
+        a = Polygon(interior).area
+        if a > 10e-06:
+            good_loops.append(interior)
+            # c = interior.centroid.wkt
+    print "Station #{0} has {1} good interior loops.".format(station.station_num, len(good_loops))
+    # list_of_cx = []
+    for loop in good_loops:
+        cx = loop.centroid.x
+        # list_of_cx.append(cx)
+        print "  unsorted x-coord of centroid is {0}".format(cx)
+    # sort the loops by the x-coordinate of their centroids, smallest to largest
+    good_loops.sort(key=attrgetter('centroid.x'))
+    for loop in good_loops:
+        cx = loop.centroid.x
+        print "  SORTED x-coord of centroid is {0}".format(cx)
 
 # m.plot_selected_cross_sections(plot_edges=False, plot_parts=True)
