4c86e2c9ca9100be718df8c8590d10da9ca27c22
Perry Roth-Johnson <perryjohnson@ucla.edu>
2013-10-14 17:34:45 -0700

    fixed y-axis title on percent mass plot

--- lib/blade.py
+++ lib/blade.py
@@ -803,7 +803,7 @@ class _Blade:
                 pa['LE panel'] + pa['shear webs (biax)'] + pa['shear webs (foam)'] +
                 pa['TE reinforcement (uniax)'] + pa['TE reinforcement (foam)'] +
                 pa['internal surfaces (triax)']))
-        plt.ylabel('percent area')
+        plt.ylabel('percent mass (per unit length)')
         plt.xlabel('blade station')
         plt.xticks(ind+width/2.0, ('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34'))
         plt.yticks( np.arange(0.0,1.1,0.1), ('0%', '10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'))
