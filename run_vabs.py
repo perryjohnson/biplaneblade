c2d906951fb5afe1436ba79cd7a5c6ab279f6e66
Perry Roth-Johnson <perryjohnson@ucla.edu>
2014-04-03 13:22:53 -0700

    calculated VABS stiffnesses for stn28

--- run_vabs.py
+++ run_vabs.py
@@ -11,7 +11,7 @@ import os
 
 # -----------------------------------------------
 # update these parameters!
-station_num = 27
+station_num = 28
 # -----------------------------------------------
 
 stn_str = 'stn{0:02d}'.format(station_num)
