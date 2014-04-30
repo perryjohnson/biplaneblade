c5db9c6861ec73ecbc2de2ab6f7a6776c4756f34
Perry Roth-Johnson <perryjohnson@ucla.edu>
2014-04-30 16:29:13 -0700

    calculated VABS props for biplane stn19

--- biplane_blade_lib/run_vabs.py
+++ biplane_blade_lib/run_vabs.py
@@ -11,7 +11,7 @@ import os
 
 # -----------------------------------------------
 # update these parameters!
-station_num = 18
+station_num = 19
 # -----------------------------------------------
 
 stn_str = 'stn{0:02d}'.format(station_num)
